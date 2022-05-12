cfc3f6cdcab0c7478d810a4e648610e6a7413d77
Leo Laporte <leo@laporte.email>
2022-05-12 16:49:47 -0700

    refinements

--- customizations/misc.el
+++ customizations/misc.el
@@ -5,11 +5,14 @@
 ;; https://magit.vc
 (straight-use-package 'magit)
 
-;; Changes all yes/no questions to y/n type
-(fset 'yes-or-no-p 'y-or-n-p)
+;; notmuch email
+(require 'notmuch)
 
-;; shell scripts
-(setq-default sh-basic-offset 2)
+;; Changes all yes/no questions to y/n type
+ (fset 'yes-or-no-p 'y-or-n-p)
+
+ ;; shell scripts
+ (setq-default sh-basic-offset 2)
 (setq-default sh-indentation 2)
 
 ;; No need for ~ files when editing
