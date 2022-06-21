08fbe5e33dd56d1ca57caa35a7f105792c5611bb
Leo Laporte <leo@laporte.email>
2022-06-20 17:30:57 -0700

    more org-roam config

--- customizations/org-mode.el
+++ customizations/org-mode.el
@@ -4,6 +4,16 @@
 ;; make sure the latest org is installed before anything calls it
 (straight-use-package 'org)
 
+(setq org-tag-alist '(("@abby" . ?a)
+		      ("@family" . ?f)
+		      ("@finance" . ?$)
+		      ("@health" . ?e)
+		      ("@home" . ?h)
+		      ("@lisa" . ?l)
+		      ("@personal" . ?p)
+		      ("@travel" . ?t)
+		      ("@work" . ?w)))
+
 ;; load org-roam
 (use-package org-roam
   :straight t
@@ -20,14 +30,29 @@
          ;; Dailies
          ("C-c n j" . org-roam-dailies-capture-today)
 	 :map org-mode-map
-	 ("C-M-i"   . completion-at-point))
-  :config
+	 ("C-M-i"   . completion-at-point)))
 
-  ;; If you're using a vertical completion framework, you might want a more informative completion interface
-  (setq org-roam-node-display-template
+;; If you're using a vertical completion framework, you might want a more informative completion interface
+(setq org-roam-node-display-template
 	(concat "${title:*} "
 		(propertize "${tags:10}" 'face 'org-tag)))
-  (setq org-roam-completion-everythere t)
-  (org-roam-db-autosync-mode)
-  ;; If using org-roam-protocol
-  (require 'org-roam-protocol))
+
+(setq org-roam-completion-everythere t)
+(org-roam-db-autosync-mode)
+
+(require 'org-roam-protocol)
+
+(setq org-roam-mode-sections
+(list #'org-roam-backlinks-section
+      #'org-roam-reflinks-section
+      ;; #'org-roam-unlinked-references-section
+      ))
+
+;; Org-roam does not control how the pop-up buffer is displayed: this is left to the user. The author’s recommended configuration is as follows:
+(add-to-list 'display-buffer-alist
+             '("\\*org-roam\\*"
+               (display-buffer-in-direction)
+               (direction . right)
+               (window-width . 0.33)
+               (window-height . fit-window-to-buffer)))
+
