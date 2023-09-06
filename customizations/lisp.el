841e3d11dba40b75aecd5f4841c1d3e521765b72
Leo Laporte <leo@laporte.email>
2023-09-05 22:11:28 -0700

    fixup projectile

--- customizations/lisp.el
+++ customizations/lisp.el
@@ -69,35 +69,3 @@
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
-
-;;;; Bits from Mark Triggs's .emacs
-;;;; Just a couple of things I find useful for Lisp editing.
-;;;; https://www.cliki.net/Bits%20from%20Mark%20Triggs%27s%20.emacs
-
-;; browser for display of hyperspec pages
-(straight-use-package 'w3m) ; w3m must be separately installed on system
-(require 'w3m)
-
-(defadvice common-lisp-hyperspec (around hyperspec-lookup-w3m () activate)
-  "Browse the Common Lisp HyperSpec using w3m.
-When leaving w3m, restore the original window configuration."
-  (let* ((window-configuration (current-window-configuration))
-         (browse-url-browser-function
-          `(lambda (url new-window)
-             (unless (member (current-buffer) (w3m-list-buffers))
-               (select-window (split-window-vertically)))
-             (w3m-browse-url url nil)
-             (let ((hs-map (copy-keymap w3m-mode-map)))
-               (define-key hs-map (kbd "q")
-                 (lambda ()
-                   (interactive)
-                   (kill-buffer nil)
-                   (set-window-configuration ,window-configuration)))
-               (use-local-map hs-map)))))
-    ad-do-it))
-
-(defun hyperspec-lookup--hyperspec-lookup-w3m (orig-fun &rest args)
-  (let ((browse-url-browser-function 'w3m-browse-url))
-    (apply orig-fun args)))
-
-(advice-add 'hyperspec-lookup :around #'hyperspec-lookup--hyperspec-lookup-w3m)
