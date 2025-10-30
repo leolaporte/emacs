eff3ca6c33b251cfa92e699f313c879437388adb
Leo Laporte <leo@laporte.email>
2025-10-30 13:38:45 -0700

    update config - including vterm, lispy

--- customizations/shell-integration.el
+++ customizations/shell-integration.el
@@ -1,9 +1,8 @@
-;; Sets up shell integration
 ;; including some macOS specific mods
 ;; modified to use  vterm - Léo 2/24/21
 
 ;; my preferred terminal emulator package - requires emacs built with module support
-;; (use-package vterm)
+(use-package vterm)
 
 (when (memq window-system '(mac ns x))
   "Set $MANPATH, $PATH and exec-path from your shell when executed in a GUI frame on OS X or Linux.
@@ -24,14 +23,14 @@
 (setenv "SHELL" shell-file-name)
 (add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m)
 
-;; (defun visit-term-buffer ()
-;;   "Create or visit a terminal buffer."
-;;   (interactive)
-;;   (if (not (get-buffer "*vterm*"))
-;;       (progn
-;;         (split-window-sensibly (selected-window))
-;;         (other-window 1)
-;;         (vterm (getenv "SHELL")))
-;;     (switch-to-buffer-other-window "*vterm*")))
+(defun visit-term-buffer ()
+ "Create or visit a terminal buffer."
+   (interactive)
+  (if (not (get-buffer "*vterm*"))
+      (progn
+         (split-window-sensibly (selected-window))
+        (other-window 1)
+        (vterm (getenv "SHELL")))
+             (switch-to-buffer-other-window "*vterm*")))
 
-;; (global-set-key (kbd "C-c t") #'visit-term-buffer) ; launch vterm
+(global-set-key (kbd "C-c t") #'visit-term-buffer) ; launch vterm
