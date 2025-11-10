c34b5a26aa9f35d13c9b564aae5a478013721780
Leo Laporte <leo@laporte.email>
2025-11-09 20:05:28 -0800

    always open maximized

--- customizations/ui.el
+++ customizations/ui.el
@@ -9,28 +9,8 @@
 ;; set default font and size
 (set-face-attribute 'default nil :family "Iosevka Nerd Font Mono" :height 220)
 
-;; rightsize window
-(setq initial-frame-alist '((top . 50) (left . 50)))
-
-(defun set-frame-size-according-to-resolution ()
-  (interactive)
-  (if window-system
-      (progn
-        ;; use 120 char wide window for largeish displays
-        ;; and smaller 80 column windows for smaller displays
-        ;; pick whatever numbers make sense for you
-        (if (> (x-display-pixel-width) 1280)
-            (add-to-list 'default-frame-alist (cons 'width 120))
-          (add-to-list 'default-frame-alist (cons 'width 80)))
-        ;; for the height, subtract a couple hundred pixels
-        ;; from the screen height (for panels, menubars and
-        ;; whatnot), then divide by the height of a char to
-        ;; get the height we want
-        (add-to-list 'default-frame-alist
-                     (cons 'height (/ (- (x-display-pixel-height) 200)
-                                      (frame-char-height)))))))
-
-(set-frame-size-according-to-resolution)
+;; open maximized
+(push '(fullscreen . maximized) default-frame-alist)
 
 ;;; use Modus Operandi high contrast theme - built-into Emacs >28.1
 ;;; https://protesilaos.com/emacs/modus-themes
